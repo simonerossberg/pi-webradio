d0de5a8e27ad5b488b5a35bbc97719f1047a07e5
simonerossberg <115003475+simonerossberg@users.noreply.github.com>
2023-02-23 15:07:52 +0100

    Update wr_funcs.js

--- files/usr/local/lib/webradio/web/js/wr_funcs.js
+++ files/usr/local/lib/webradio/web/js/wr_funcs.js
@@ -169,18 +169,42 @@ function scroll_to_current_file() {
   Update functions for all clocks
 */
 
-document.addEventListener('DOMContentLoaded', () =>
-  requestAnimationFrame(updateTime)
-)
+let size = 86;
+let columns = Array.from(document.getElementsByClassName('column'));
+let d, c;
+let classList = [ 'visible', 'close', 'far', 'far', 'distant', 'distant' ];
+let use24HourClock = true;
 
-function updateTime() {
-  document.documentElement.style.setProperty('--timer-day', "'" + moment().format("dd") + "'");
-  document.documentElement.style.setProperty('--timer-hours', "'" + moment().format("k") + "'");
-  document.documentElement.style.setProperty('--timer-minutes', "'" + moment().format("mm") + "'");
-  document.documentElement.style.setProperty('--timer-seconds', "'" + moment().format("ss") + "'");
-  requestAnimationFrame(updateTime);
+function padClock(p, n) {
+	return p + ('0' + n).slice(-2);
 }
 
+function getClock() {
+	d = new Date();
+	return [
+			use24HourClock ? d.getHours() : (d.getHours() % 12 || 12),
+			d.getMinutes(),
+			d.getSeconds()
+		]
+		.reduce(padClock, '');
+}
+
+function getClass(n, i2) {
+	return classList.find((class_, classIndex) => Math.abs(n - i2) === classIndex) || '';
+}
+
+let loop = setInterval(() => {
+	c = getClock();
+
+	columns.forEach((ele, i) => {
+		let n = +c[i];
+		let offset = -n * size;
+		ele.style.transform = `translateY(calc(50vh + ${offset}px - ${size / 2}px))`;
+		Array.from(ele.children).forEach((ele2, i2) => {
+			ele2.className = 'num ' + getClass(n, i2);
+		});
+	});
+}, 200 + Math.E * 10);
 /**
   Show message in message-area
 */
