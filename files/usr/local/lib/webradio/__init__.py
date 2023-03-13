b56f79fb31dce90f5184b5c292132e5144d82a62
simonerossberg <115003475+simonerossberg@users.noreply.github.com>
2023-03-13 12:05:17 +0100

    Update __init__.py

--- files/usr/local/lib/webradio/__init__.py
+++ files/usr/local/lib/webradio/__init__.py
@@ -4,11 +4,6 @@
 # Pi-Webradio application module. The file just imports all classes
 # into the wstation namespace
 #
-# Author: Bernhard Bablok
-# License: GPL3
-#
-# Website: https://github.com/bablokb/pi-webradio
-#
 # ----------------------------------------------------------------------------
 
 from . SRBase           import Base           as Base
@@ -18,12 +13,12 @@ from . SRRadioEvents    import RadioEvents    as RadioEvents
 from . SRRadio          import Radio          as Radio
 from . SRPlayer         import Player         as Player
 from . SRRecorder       import Recorder       as Recorder
+from . SRBluetooth      import Bluetooth      as Bluetooth
 from . SRMpg123         import Mpg123         as Mpg123
 from . SRWebServer      import WebServer      as WebServer
 from . SRWebRadio       import WebRadio       as WebRadio
 from . SRRadioClient    import RadioClient    as RadioClient
 from . SRKeyController  import KeyController  as KeyController
-from . SRBluetooth      import Bluetooth      as Bluetooth
 
 # voice control with Vosk is optional
 have_vosk = False
