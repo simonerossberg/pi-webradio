acaa6dec27fa3d13dceb9903cd3f29ba923d22c1
simonerossberg <115003475+simonerossberg@users.noreply.github.com>
2023-03-22 11:47:17 +0100

    Update SRPlayer.py

--- files/usr/local/lib/webradio/SRPlayer.py
+++ files/usr/local/lib/webradio/SRPlayer.py
@@ -6,7 +6,7 @@
 # The class Player implements the playback device for existing recordings
 #
 # Quelle: (Author: Bernhard Bablok, License: GPL3, Website: https://github.com/bablokb/pi-webradio)
-#
+# Bearbeitet: Simone Roßberg
 # -----------------------------------------------------------------------------
 
 import os, time, datetime, subprocess, threading, copy, queue
@@ -14,10 +14,10 @@ import os, time, datetime, subprocess, threading, copy, queue
 from webradio import Base
 
 class Player(Base):
-  """ Player-controller """
+  """ Player-Controller """
 
   def __init__(self,app):
-    """ initialization """
+    """ Initialisierung """
 
     self._app     = app
     self.debug    = app.debug
@@ -34,12 +34,12 @@ class Player(Base):
     self.read_config()
     self.register_apis()
 
-  # --- read configuration   --------------------------------------------------
+  # --- Konfiguration lesen   --------------------------------------------------
 
   def read_config(self):
-    """ read configuration from config-file """
-
-    # section [PLAYER]
+    """ Konfiguration aus Konfigurationsdatei lesen """
+Standardverzeichnis
+    # Section [PLAYER]
     self._wait_dir = int(self.get_value(self._app.parser,"PLAYER",
                                         "player_wait_dir",10))
     self._root_dir = self.get_value(self._app.parser,"PLAYER",
@@ -53,13 +53,13 @@ class Player(Base):
     self._def_dir = os.path.abspath(self._def_dir)
 
     self._dir = self._def_dir
-    self.msg("Player: root dir:    %s" % self._root_dir)
-    self.msg("Player: default dir: %s" % self._def_dir)
+    self.msg("Player: Stammverzeichnis:    %s" % self._root_dir)
+    self.msg("Player: Standardverzeichnis: %s" % self._def_dir)
 
-  # --- register APIs   ------------------------------------------------------
+  # --- Register APIs   ------------------------------------------------------
 
   def register_apis(self):
-    """ register API-functions """
+    """ Register API-Funktionen """
 
     self._api.player_play_file  = self.player_play_file
     self._api.player_stop       = self.player_stop
@@ -70,102 +70,102 @@ class Player(Base):
     self._api.player_play_dir   = self.player_play_dir
     self._api._player_get_cover_file = self._player_get_cover_file
 
-  # --- return persistent state of this class   -------------------------------
+  # --- gibt den persistenten Zustand dieser Klasse zurück   -------------------------------
 
   def get_persistent_state(self):
-    """ return persistent state (overrides SRBase.get_pesistent_state()) """
+    """ Persistenten Zustand zurückgeben (überschreibt SRBase.get_pesistent_state()) """
     return {
       'player_dir': self._dir,
       'player_file': self._file
       }
 
-  # --- restore persistent state of this class   ------------------------------
+  # --- den persistenten Zustand dieser Klasse wiederherstellen  ------------------------------
 
   def set_persistent_state(self,state_map):
-    """ restore persistent state (overrides SRBase.set_pesistent_state()) """
+    """ persistenten Zustand wiederherstellen (überschreibt SRBase.set_pesistent_state()) """
 
-    self.msg("Player: restoring persistent state")
+    self.msg("Player: dauerhaften Zustand wiederherstellen")
     if 'player_dir' in state_map:
       self._dir = state_map['player_dir']
-      self.msg("Player: currrent dir (tentative):  %s" % self._dir)
+      self.msg("Player: aktuelles Verzeichnis (vorläufig):  %s" % self._dir)
     if 'player_file' in state_map:
       self._file = state_map['player_file']
-      self.msg("Player: currrent file (tentative): %s" % self._file)
+      self.msg("Player: aktuelle Datei (vorläufig): %s" % self._file)
     self._api.update_state(section="player",key="last_dir",
                            value=self._dir[len(self._root_dir):]+os.path.sep,
                            publish=False)
     self._init_thread = threading.Thread(target=self._init_state)
     self._init_thread.start()
 
-  # --- lazy query of dir-info during initialization   ----------------------
+  # --- Abfrage von dir-info während der Initialisierung   ----------------------
 
   def _init_state(self):
-    """ wait for directory and query dir-info """
+    """ Warten auf das Verzeichnis und abfragen Dir-info """
 
-    # check directory (wait if necessary)
-    self.msg("Player: waiting for %s" % self._dir)
+    # Verzeichnis prüfen (ggf. warten)
+    self.msg("Player: warten auf %s" % self._dir)
     while not os.path.exists(self._dir) and self._wait_dir:
       time.sleep(1)
       self._wait_dir -= 1
 
-    # check again
+    # Nochmal Überprüfen
     if self._check_dir(self._dir):
       self._get_dirinfo(self._dir,True)
 
     else:
-      # oops, check failed, now check everything
+      # Hoppla, Überprüfung fehlgeschlagen, jetzt alles überprüfen
       if not os.path.exists(self._root_dir):
-        self.msg("[WARNING] Player: root-directory %s of player does not exist" %
+        self.msg("[WARNING] Player: Verzeichnis %s des Players existiert nicht" %
                  self._root_dir,True)
         self._root_dir = os.path.expanduser("~")
-        self.msg("[WARNING] Player: using %s as fallback" % self._root_dir,True)
+        self.msg("[WARNING] Player: verwendet %s als Rückfall" % self._root_dir,True)
       if not self._check_dir(self._def_dir):
         self._def_dir = self._root_dir
-        self.msg("[WARNING] Player: using %s as fallback" % self._root_dir,True)
+        self.msg("[WARNING] Player: verwendet %s als Rückfall" % self._root_dir,True)
       self._dir = self._def_dir
       self._get_dirinfo(self._dir,True)
 
-    # also check files now
+    # jetzt auch Dateien prüfen
     if self._file and not self._check_file(self._file):
       self._file = None
 
     self._init_thread = None
-    self.msg("Player: currrent dir:  %s" % self._dir)
-    self.msg("Player: currrent file: %s" % self._file)
+    self.msg("Player: aktuelle dir:  %s" % self._dir)
+    self.msg("Player: aktuelle Datei: %s" % self._file)
 
-  # --- check directory   ---------------------------------------------------
+  # --- Verzeichnis prüfen   ---------------------------------------------------
 
   def _check_dir(self,path):
-    """ check if directory is valid """
+    """ Prüfen, ob das Verzeichnis gültig ist """
 
     path = os.path.abspath(path)
     if not os.path.exists(path):
-      self.msg("[WARNING] Player: %s does not exist" % path)
+      self.msg("[WARNING] Player: %s ist nicht vorhanden" % path)
       return False
 
     if not os.path.commonpath([self._root_dir,path]) == self._root_dir:
-      self.msg("[WARNING] Player: %s is not child of root-directory" % path,
+      self.msg("[WARNING] Player: %s ist kein untergeordnetes Element des Stammverzeichnisse" % path,
                True)
       return False
 
     return True
 
-  # --- check file   ---------------------------------------------------------
+  # --- Datei prüfen   ---------------------------------------------------------
 
   def _check_file(self,path):
-    """ check if file is valid """
+    """ Überprüfen Sie, ob die Datei gültig ist"""
 
     path = os.path.abspath(path)
     if not os.path.exists(path):
-      self.msg("[WARNING] Player: %s does not exist" % path)
+      self.msg("[WARNING] Player: %s ist nicht vorhanden" % path)
       return False
     else:
       return self._check_dir(os.path.dirname(path))
 
-  # --- pretty print duration/time   ----------------------------------------
+  # --- Druckdauer/-zeit   ----------------------------------------
 
   def _pp_time(self,seconds):
-    """ pritty-print time as mm:ss or hh:mm """
+    """ Druckzeit als mm:ss oder hh:mm """
 
     m, s = divmod(seconds,60)
     h, m = divmod(m,60)
@@ -174,10 +174,10 @@ class Player(Base):
     else:
       return "{0:02d}:{1:02d}".format(m,s)
 
-  # --- start playing   -------------------------------------------------------
+  # --- start spielen   -------------------------------------------------------
 
   def player_play_file(self,file=None,last=True):
-    """ start playing """
+    """ start spielen """
 
     if self._init_thread:
       self._init_thread.join()
@@ -195,9 +195,9 @@ class Player(Base):
     if self._dirinfo:
       self._dirinfo['cur_file'] = self._file
 
-    # this will push the information to all clients, even if the file
-    # is already playing.
-    # We might also need to push the elapsed time?!
+    # Dadurch werden die Informationen an alle Clients übertragen, auch wenn die Datei
+    # spielt schon
+    # Möglicherweise müssen wir auch die verstrichene Zeit verschieben?!
 
     total_secs = int(subprocess.check_output(["mp3info", "-p","%S",self._file]))
     file_info = {'name': os.path.basename(self._file),
@@ -213,44 +213,44 @@ class Player(Base):
                            publish=False)
     return file_info
 
-  # --- stop playing   -------------------------------------------------------
+  # --- Stop spielen   -------------------------------------------------------
 
   def player_stop(self):
-    """ stop playing (play->stop, pause->stop)"""
+    """ Stop spielen (play->stop, pause->stop)"""
 
     if self._dirplay:
-      self._dirstop.set()         # this will also stop the backend
+      self._dirstop.set()         # dadurch wird auch das Backend gestoppt
       self._dirplay.join()
     else:
-      self._backend.stop()        # backend will publish eof-event
+      self._backend.stop()        # Backend wird eof-event veröffentlichen
 
-  # --- pause playing   -----------------------------------------------------
+  # --- Pausiert spielen   -----------------------------------------------------
 
   def player_pause(self):
-    """ pause playing (play->pause) """
+    """ pausiert playing (play->pause) """
 
     self._backend.pause()
 
-  # --- resume playing   ----------------------------------------------------
+  # --- weiter spielen   ----------------------------------------------------
 
   def player_resume(self):
-    """ resume playing (pause->play) """
+    """ weiter spielen (pause->play) """
 
     self._backend.resume()
 
-  # --- toggle playing   ------------------------------------------------------
+  # --- Abspielen umschalten   ------------------------------------------------------
 
   def player_toggle(self):
-    """ toggle playing (play->pause, pause->play) """
+    """ Abspielen umschalten (play->pause, pause->play) """
 
     self._backend.toggle()
 
-  # --- select directory, return entries   ------------------------------------
+  # --- Verzeichnis auswählen, Einträge zurückgeben   ------------------------------------
 
   def player_select_dir(self,dir=None):
-    """ select directory:
-        a directory starting with a / is always interpreted relative
-        to root_dir, otherwise relative to the current directory
+    """ Verzeichnis auswählen:
+        ein Verzeichnis, das mit einem / beginnt, wird immer relativ interpretiert
+        auf root_dir, ansonsten relativ zum aktuellen Verzeichnis
     """
 
     if self._init_thread:
@@ -259,15 +259,15 @@ class Player(Base):
     self._lock.acquire()
 
     if not dir:
-      # use current directory, keep current file
+      # aktuelles Verzeichnis verwenden, aktuelle Datei behalten
       dir = self._dir
     else:
       if os.path.isabs(dir):
         dir = os.path.normpath(self._root_dir+dir)   # cannot use join here!
-        self.msg("Player: dir is absolute, fullpath %s" % dir)
+        self.msg("Player: dir ist absolut, vollständiger Pfad %s" % dir)
       else:
         dir = os.path.normpath(os.path.join(self._dir,dir))
-        self.msg("Player: dir is relative, fullpath %s" % dir)
+        self.msg("Player: dir ist relativ, vollständiger Pfad %s" % dir)
       if not self._check_dir(dir):
         self._lock.release()
         raise ValueError("invalid directory %s" % dir)
@@ -277,62 +277,62 @@ class Player(Base):
       if self._dirinfo:
         cache_valid = True
     else:
-      # set new current directory
+      # neues aktuelles Verzeichnis setzen
       self._dir = dir
 
-    # publish event first (return dir relative to root_dir)
+    # Ereignis zuerst veröffentlichen (Verzeichnis relativ zu root_dir zurückgeben)
     cur_dir = self._dir[len(self._root_dir):]+os.path.sep
     self._api._push_event({'type':  'dir_select', 'value': cur_dir})
     self._api.update_state(section="player",key="last_dir",
                            value=cur_dir,
                            publish=False)
 
-    # then query new directory info
+    # dann neue Verzeichnisinfo abfragen
     if not cache_valid:
       self._get_dirinfo(dir)
       self._dirinfo['cur_dir'] = cur_dir
     else:
-      self.msg("Player: using cached dir-info for %s" % dir)
+      self.msg("Player: Verwenden von zwischengespeicherten Verzeichnisinformationen für %s" % dir)
 
     self._lock.release()
     return self._dirinfo
 
-  # --- play all files in directory   -----------------------------------------
+  # --- Alle Dateien im Verzeichnis abspielen   -----------------------------------------
 
   def player_play_dir(self,start=None):
-    """ play all files in the current directory starting with
-        the given file
+    """ alle Dateien im aktuellen Verzeichnis beginnend mit abspielen
+        die angegebene Datei
     """
 
     if self._init_thread:
       self._init_thread.join()
 
-    # check existing player-thread, stop it and wait until it is finished
+    # Überprüfen Sie den vorhandenen Player-Thread, stoppen Sie ihn und warten Sie, bis er beendet ist
     if self._dirplay:
       self._dirstop.set()
       self._dirplay.join()
 
-    # copy file-list
+    # Dateiliste kopieren
     if not start:
       files = copy.deepcopy(self._dirinfo['files'])
     else:
       try:
         index = self._dirinfo['files'].index(start)
-        self.msg("Player: starting play_dir with file %s (index %i)" %
+        self.msg("Player: starte play_dir mit Datei %s (index %i)" %
                  (start,index))
         files = copy.deepcopy(self._dirinfo['files'][index:])
       except ValueError:
         raise ValueError("file %s does not exist" % start)
 
-    # start player-thread, pass files as argument
+    # Player-Thread starten, Dateien als Argument übergeben
     self._dirstop.clear()
     self._dirplay = threading.Thread(target=self._play_dir,args=(files,))
     self._dirplay.start()
 
-  # --- play all files (helper)   --------------------------------------------
+  # --- Alle Dateien abspielen (Helfer)   --------------------------------------------
 
   def _play_dir(self,files):
-    """ play all given files """
+    """ alle angegebenen Dateien abspielen """
 
     ev_queue = self._api._add_consumer("_play_dir")
     do_exit = False
@@ -341,11 +341,10 @@ class Player(Base):
     for index,fname in enumerate(files):
       if do_exit:
         break
-      self.msg("Player: _play_dir: playing next file %s" % fname)
+      self.msg("Player: _play_dir: nächste Datei abspielen %s" % fname)
       self.player_play_file(fname,last=index==index_last)
       while True:
-        # a naive implementation would just block on the queue, but
-        # then we could stop this thread only after an event occurs
+        """ Implementierung würde nur Warteschlange blockieren, dann könnten wir diesen Thread erst stoppen, nachdem ein Ereignis eintritt """
         if self._dirstop.wait(timeout=1.0):
           do_exit = True
           break
@@ -354,8 +353,8 @@ class Player(Base):
           ev_queue.task_done()
           if ev:
             if ev['type'] == 'eof' and ev['value']['name'] == fname:
-              self.msg("Player: processing eof for %s" % fname)
-              break                              # start next file
+              self.msg("Player: Verarbeitung von eof für %s" % fname)
+              break                              # Nächste Datei starten
           else:
             do_exit = True
             break
@@ -363,15 +362,15 @@ class Player(Base):
           pass
 
     # cleanup
-    self.msg("Player: stopping _play_dir and cleaning up")
+    self.msg("Player: _play_dir stoppen und aufräumen")
     self._api._del_consumer("_play_dir")
     self._backend.stop()
     self._dirplay = None
 
-  # --- return name of cover file (currently only cover.jpg)   ---------------
+  # --- Rückgabename der Cover-Datei (currently only cover.jpg)   ---------------
 
   def _player_get_cover_file(self):
-    """ return name of cover file """
+    """ Rückgabename der Cover-Datei """
 
     cover = os.path.join(self._dir,"cover.jpg")
     if os.path.exists(cover):
@@ -379,15 +378,15 @@ class Player(Base):
     else:
       return None
 
-  # --- create directory info for given dir   --------------------------------
+  # --- Erstellen der Verzeichnisinformationen für das angegebene Verzeichnis   --------------------------------
 
   def _get_dirinfo(self,dir,init=False):
-    """ create directory info """
+    """ Verzeichnisinformationen erstellen """
 
     self._dirinfo =  {'dirs':  [], 'files': [], 'dur': []}
-    self.msg("Player: collecting dir-info for %s" % dir)
+    self.msg("Player: Sammeln dir-info für %s" % dir)
 
-    # first entry is parent directory
+    # Der erste Eintrag ist das übergeordnete Verzeichnis
     if self._dir != self._root_dir:
       self._dirinfo['dirs'].append('..')
 
@@ -398,11 +397,11 @@ class Player(Base):
       else:
         self._dirinfo['dirs'].append(f)
 
-    # ... and sort results
+    # ... Ergebnisse sortieren
     self._dirinfo['files'].sort()
     self._dirinfo['dirs'].sort()
 
-    # set current file
+    # aktuelle Datei setzen
     if self._file and init:
       self._dirinfo['cur_file'] = os.path.basename(self._file)
     else:
@@ -414,7 +413,7 @@ class Player(Base):
       self._api.update_state(section="player",key="last_file",
                              value= self._dirinfo['cur_file'],publish=False)
 
-    # add add time-info
+    # Zeit-Info hinzufügen
     for f in self._dirinfo['files']:
       secs = int(subprocess.check_output(["mp3info",
                                           "-p","%S",
