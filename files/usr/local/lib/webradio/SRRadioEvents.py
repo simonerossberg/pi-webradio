899950f127613e1771e5942bf7d47c76c7c715ef
simonerossberg <115003475+simonerossberg@users.noreply.github.com>
2023-03-24 14:28:48 +0100

    Update SRRadioEvents.py

--- files/usr/local/lib/webradio/SRRadioEvents.py
+++ files/usr/local/lib/webradio/SRRadioEvents.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 # -----------------------------------------------------------------------------
-# Pi-Webradio: implementation of class RadioEvents
+# Pi-Webradio: Implementierung der Klasse RadioEvents
 #
-# The class RadioEvents multiplexes events to multiple consumers.
+# Die Klasse RadioEvents multiplext Ereignisse an mehrere Verbraucher.
 #
-# Author: Bernhard Bablok
-# License: GPL3
-#
-# Website: https://github.com/bablokb/pi-webradio
+# Quelle: (Author: Bernhard Bablok, License: GPL3, Website: https://github.com/bablokb/pi-webradio)
+# Bearbeitet: Simone Roßberg
 #
 # -----------------------------------------------------------------------------
 
@@ -18,13 +16,13 @@ from webradio import Base
 from webradio import EventFormatter
 
 class RadioEvents(Base):
-  """ Multiplex events to consumers """
+  """ Multiplex-Ereignisse für Verbraucher """
 
-  QUEUE_SIZE          = 20   # size of client event-queues
-  KEEP_ALIVE_INTERVAL = 15   # send keep-alive every x seconds
+  QUEUE_SIZE          = 20   # Größe der Client-Ereigniswarteschlangen
+  KEEP_ALIVE_INTERVAL = 15   # sende Keep-Alive alle x Sekunden
 
   def __init__(self,app):
-    """ initialization """
+    """ Initialisierung """
 
     self._api         = app.api
     self.debug        = app.debug
@@ -36,7 +34,7 @@ class RadioEvents(Base):
     self.register_apis()
     threading.Thread(target=self._process_events).start()
 
-  # --- register APIs   ------------------------------------------------------
+  # --- Register APIs   ------------------------------------------------------
 
   def register_apis(self):
     """ register API-functions """
@@ -45,23 +43,23 @@ class RadioEvents(Base):
     self._api._add_consumer = self.add_consumer
     self._api._del_consumer = self.del_consumer
 
-  # --- push an event to the input queue   -----------------------------------
+  # --- Pushen Sie ein Ereignis in die Eingabewarteschlang   -----------------------------------
 
   def push_event(self,event):
-    """ push event to the  input queue """
+    """ Push-Ereignis in die Eingabewarteschlange """
 
     self._input_queue.put(event)
 
-  # --- add a consumer   -----------------------------------------------------
+  # --- Verbraucher hinzufügen   -----------------------------------------------------
 
   def add_consumer(self,id):
-    """ add a consumer to the list of consumers """
+    """ Hinzufügen eines Verbrauchers zur Liste der Verbraucher """
 
     if id in self._consumers:
-      self.msg("RadioEvents: reusing consumer-queue with id %s" % id)
+      self.msg("RadioEvents: Wiederverwendung der Verbraucherwarteschlange mit id %s" % id)
       return self._consumers[id]
     else:
-      self.msg("RadioEvents: adding consumer with id %s" % id)
+      self.msg("RadioEvents: Verbraucher hinzufügen mit id %s" % id)
       with self._lock:
         self._consumers[id] = queue.Queue(RadioEvents.QUEUE_SIZE)
       try:
@@ -77,35 +75,34 @@ class RadioEvents(Base):
           del self._consumers[id]
         return None
 
-  # --- remove a consumer   --------------------------------------------------
+  # --- einen Verbraucher entfernen   --------------------------------------------------
 
   def del_consumer(self,id):
-    """ delete a consumer from the list of consumers """
+    """ Löschen eines Verbrauchers aus der Liste der Verbraucher """
 
     if id in self._consumers:
       self._consumers[id].put(None)
       with self._lock:
         del self._consumers[id]
 
-  # --- multiplex events   ---------------------------------------------------
+  # --- Multiplex-Ereignisse   ---------------------------------------------------
 
   def _process_events(self):
-    """ pull events from the input-queue and distribute to the consumer queues """
+    """ Zieht Ereignisse aus der Eingabewarteschlange und verteilt sie an die Verbraucherwarteschlangen """
 
-    self.msg("RadioEvents: starting event-processing")
+    self.msg("RadioEvents: Ereignisverarbeitung starten")
     count = 0
     while not self._stop_event.is_set():
       try:
         event = self._input_queue.get(block=True,timeout=1)   # block 1s
         self._input_queue.task_done()
-        self.msg("RadioEvents: received event: %r" % (event,))
+        self.msg("RadioEvents: erhaltenes Ereignis: %r" % (event,))
         count = 0
       except queue.Empty:
         count = (count+1) % RadioEvents.KEEP_ALIVE_INTERVAL
         if count > 0:
           continue
         else:
-          #self.msg("RadioEvents: publishing keep-alive")
           event = {'type': 'keep_alive', 'value':
                    datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
 
@@ -117,16 +114,16 @@ class RadioEvents(Base):
         except queue.Full:
           stale_consumers.append(id)
 
-      # delete stale consumers
+      # veraltete Verbraucher löschen
       with self._lock:
         for id in stale_consumers:
-          self.msg("RadioEvents: deleting stale queue with id %s" % id)
+          self.msg("RadioEvents: Löschen veralteter Warteschlangen mit id %s" % id)
           del self._consumers[id]
 
-    self.msg("RadioEvents: stopping event-processing")
+    self.msg("RadioEvents: Stoppen der Ereignisverarbeitung")
     for consumer in self._consumers.values():
       try:
         consumer.put_nowait(None)
       except:
         pass
-    self.msg("RadioEvents: event-processing finished")
+    self.msg("RadioEvents: Ereignisverarbeitung abgeschlossen")
